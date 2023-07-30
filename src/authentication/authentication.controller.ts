adc474ee46dfb5f8a8980d6d7dfcde83814f1595
shahrukh <shahrukh.khan@lovebonito.com>
2023-07-31 00:10:29 +0500

    feat: signup api added

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SigninDTO } from './dtos/signin.dto';
+import { SignupDTO } from './dtos/signup.dto';
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
-  @Post()
+  @Post('signin')
   async signin(@Body() body: SigninDTO) {
     return this.authenticationService.signin(body.email, body.password);
+  }
+
+  @Post('signup')
+  async signup(@Body() body: SignupDTO) {
+    return this.authenticationService.signup(body);
   }
 }
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,11 +1,7 @@
-import {
-  Injectable,
-  UnauthorizedException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthenticationService {
   constructor(
@@ -15,8 +11,8 @@ export class AuthenticationService {
 
   async signin(email: string, pass: string) {
     const user = await this.usersService.findByEmail(email, true);
-    if (user?.password !== pass) {
-      throw new NotFoundException('Invalid Email or Password!');
+    if (!(await bcrypt.compare(pass, user.password))) {
+      throw new UnauthorizedException('Invalid Email or Password!');
     }
     const payload = { sub: user.id, email: user.email };
     return {
@@ -24,5 +20,16 @@ export class AuthenticationService {
     };
   }
 
-  signup() {}
+  async signup(user) {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    const newUser = await this.usersService.create({
+      ...user,
+      password: hashedPassword,
+    });
+
+    const payload = { sub: newUser.id, email: newUser.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
 }
